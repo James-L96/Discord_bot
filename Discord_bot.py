e5cbc295fc35919ecdff468974eff5352ce541d5
James Lincoln <jameslincoln1996@gmail.com>
2025-11-24 20:51:02 -0600

    Rough draft of some terrible bot I'm making.

---
+++ Discord_bot.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+import discord
+from discord.ext import commands
+
+# Load variables from .env
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send("Hello! I am your bot!")
+
+bot.run(TOKEN)
