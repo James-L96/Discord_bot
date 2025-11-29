f0acff644fee816149514835fead9fc1fb605706
James Lincoln <jameslincoln1996@gmail.com>
2025-11-28 20:55:08 -0600

    Basic testing of recognizing inputs.

--- Discord_bot.py
+++ Discord_bot.py
@@ -14,10 +14,18 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f'Logged in as {bot.user}')
+    print(f"Logged in as {bot.user}")
 
-@bot.command()
-async def hello(ctx):
-    await ctx.send("Hello! I am your bot!")
+@bot.event
+async def on_message(message):
+    # Prevent bot from replying to itself
+    if message.author == bot.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hey pookie bear!')
+
+    # Allow commands to still work when on_message is overridden
+    await bot.process_commands(message)
 
 bot.run(TOKEN)
