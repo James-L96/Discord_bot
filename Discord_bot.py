3fa6f814d9f2a74529508e6be27d1bc9337edd90
James Lincoln <jameslincoln1996@gmail.com>
2025-11-30 00:39:50 -0600

    Now responds to the sports API.Work on pulling news. Next work on the gaming API.

--- Discord_bot.py
+++ Discord_bot.py
@@ -2,10 +2,11 @@ import os
 from dotenv import load_dotenv
 import discord
 from discord.ext import commands
-
+import requests
 # Load variables from .env
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
+NEWS_KEY = os.getenv("NEWS_KEY")
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -25,8 +26,22 @@ async def on_message(message):
     if message.content.startswith('!hello'):
         await message.channel.send('Hey pookie bear!')
 
-
-    # Allow commands to still work when on_message is overridden
     await bot.process_commands(message)
+
+@bot.command()
+async def sports(ctx):
+    url = f"https://newsapi.org/v2/everything?q=sports&sortBy=publishedAt&apiKey={NEWS_KEY}"
+    response = requests.get(url).json()
+
+    # Get first headline
+    article = response["articles"][0]
+    title = article["title"]
+    link = article["url"]
+
+    await ctx.send(f"**Sports Update:** {title}\n🔗 {link}")
+
+@bot.command()
+async def games(ctx):
+    await ctx.send("Gaming news coming soon!")
 
 bot.run(TOKEN)
